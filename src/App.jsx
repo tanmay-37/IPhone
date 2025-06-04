c09623d833e1bd8a2d9e633c70efc876997940a6
tanmay-37 <talekartk@rknec.edu>
2025-06-04 11:45:51 +0530

    initialized and completd navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import HighLights from "./components/Hero.jsx";
+const App = () => {
+    return (
+        <main>
+            <Navbar/>
+            <Hero/>
+            <HighLights/>
+        </main>
+    )
+}
+export default App
