ceb8666d9688a52263f83b4a4de6e9d74114e0b2
tanmay-37 <talekartk@rknec.edu>
2025-06-07 11:16:04 +0530

    added 3d iphone model

--- eslint.config.js
+++ eslint.config.js
@@ -19,6 +19,7 @@ export default [
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      '@react-three' : reactThree,
     },
     rules: {
       ...js.configs.recommended.rules,
--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import HighLights from "./components/HighLights.jsx";
+import Model from "./components/Model.jsx";
 const App = () => {
     return (
         <main>
             <Navbar/>
             <Hero/>
             <HighLights/>
+            <Model/>
         </main>
     )
 }
