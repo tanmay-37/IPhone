53944628551fbfc7652f0c79a4aa90cb8ca9dc9e
tanmay-37 <talekartk@rknec.edu>
2025-06-07 17:30:26 +0530

    completed 3d model integration

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 import {useGSAP} from "@gsap/react";
 import gsap from "gsap";
 import {yellowImg} from "../utils/index.js";
@@ -7,6 +7,7 @@ import ModelView from "./ModelView.jsx";
 import {Canvas} from "@react-three/fiber";
 import { View } from "@react-three/drei";
 import {models, sizes} from "../constants/index.js";
+import {animateWithGsap, animateWithGsapTimeline} from "../utils/animation.js";
 
 const Model = () => {
 
@@ -25,6 +26,25 @@ const Model = () => {
 
     const [smallRotation, setSmallRotation] = useState(0)
     const [LargeRotation, setLargeRotation] = useState(0)
+
+    const tl = gsap.timeline();
+
+    useEffect(() => {
+        if(size === 'large') {
+            animateWithGsapTimeline(tl, small, smallRotation, '#view1', '#view2', {
+                transform: 'translateX(-100%)',
+                duration: 2
+            })
+        }
+
+        if(size ==='small') {
+            animateWithGsapTimeline(tl, large, LargeRotation, '#view2', '#view1', {
+                transform: 'translateX(0)',
+                duration: 2
+            })
+        }
+    }, [size])
+
 
     useGSAP(() => {
         gsap.to('#heading', {
